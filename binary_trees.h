9c428da01688595720870d702af3efa99eb420fa
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-11-28 04:20:52 -0800

    included print function

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,11 @@
 #ifndef binary_trees_h
 #define binary_trees_h
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,7 +27,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
