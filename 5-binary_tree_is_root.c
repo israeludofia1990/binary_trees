98c9ffc83bc81f995c9aba59a151fd8af5c40657
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-01 03:44:40 +0100

    the code

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - function that checks if a five code is a root
+ * @node: is the pointer to the node to check
+ * Return: 1 if nod is a root, otherwise 0, if is NULL, return 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
